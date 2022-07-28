168413fbbfdef3648a6cfdcfb1a5608ac062c680
Christoph Giesinger <c.giesinger@mima-poker.com>
2022-07-28 09:05:26 +0200

    tests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  
+  const name = 'Christoph'
+  const nachname = 'Giesinger'
+  const alter = 13
+  
   return (
+  
+  
+  
+  <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hallo {name} {nachname}, du bist {alter} Jahre alt</h1>
     </div>
+    </>
   );
 }
 
